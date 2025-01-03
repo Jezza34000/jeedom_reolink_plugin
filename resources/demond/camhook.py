02212f7dfb4c4a4233eeaefa9bdf2a80ac972974
Mips2648 <mips2648@gmail.com>
2025-01-03 08:17:47 +0100

    fix memory leaks + minors bugfix

--- resources/demond/camhook.py
+++ resources/demond/camhook.py
@@ -2,31 +2,29 @@ from fastapi import Request, FastAPI
 import sys
 import json
 import re
+import logging
+import os
+
+from jeedom.jeedom import jeedom_com
 
 try:
-	from jeedom.jeedom import *
-except ImportError:
-	print("Error: importing module jeedom.jeedom")
-	sys.exit(1)
-
-try:
-	f = open('jeedomcreds', 'r')
-	_callback = f.readline().rstrip("\n")
-	_apikey = f.readline().rstrip("\n")
-	f.close()
+    f = open('jeedomcreds', 'r')
+    _callback = f.readline().rstrip("\n")
+    _apikey = f.readline().rstrip("\n")
+    f.close()
 except:
-	logging.error(f"Unable to read credentials jeedom file, retry...")
-	sys.exit(1)
+    logging.error("Unable to read credentials jeedom file, retry...")
+    sys.exit(1)
 
 try:
-	os.remove("jeedomcreds")
+    os.remove("jeedomcreds")
 except:
-	pass
+    pass
 
 jeedom_cnx = jeedom_com(_apikey, _callback)
 detect_state = 0
 # BEGIN  added by t0urista to handle ONVIF events
-eventTable = {}  
+eventTable = {}
 # END  added by t0urista to handle ONVIF events
 app = FastAPI()
 
@@ -34,73 +32,72 @@ app = FastAPI()
 @app.post("/inbound_events", status_code=200)
 async def get_body(request: Request):
 
-	global detect_state
+    global detect_state
 
 # BEGIN  added by t0urista to handle ONVIF events
-	global eventTable
+    global eventTable
 # END  added by t0urista to handle ONVIF events
 
-    
-	ip = request.client.host
-	logging.debug(f"Incoming XML camera event on webhook from IP={ip}")
-	xml_answer = await request.body()
+    ip = request.client.host
+    logging.debug(f"Incoming XML camera event on webhook from IP={ip}")
+    xml_answer = await request.body()
 
-	new_detect_state = 0
-	if re.search('IsMotion" Value="true"', xml_answer.decode('utf-8')):
-		new_detect_state = 1
-	if detect_state != new_detect_state:
-		detect_state = new_detect_state
-		send_frame = {
-			"message": "motion",
-			"ip": ip,
-			"motionstate": detect_state
-		}
+    new_detect_state = 0
+    if re.search('IsMotion" Value="true"', xml_answer.decode('utf-8')):
+        new_detect_state = 1
+    if detect_state != new_detect_state:
+        detect_state = new_detect_state
+        send_frame = {
+            "message": "motion",
+            "ip": ip,
+            "motionstate": detect_state
+        }
 # convert into JSON:
-		message = json.dumps(send_frame)
-		jeedom_cnx.send_change_immediate(json.loads(message))
+        message = json.dumps(send_frame)
+        jeedom_cnx.send_change_immediate(json.loads(message))
 
 
 # BEGIN  added by t0urista to handle ONVIF events
-	pattern=r'<wsnt:.*?ConcreteSet">(.*?)</wsnt:Topic>.*?<tt:Data>.*?Value="(.*?)" /></tt:Data>'
-	event_status_all=re.findall(pattern,xml_answer.decode('utf-8'))
+    pattern = r'<wsnt:.*?ConcreteSet">(.*?)</wsnt:Topic>.*?<tt:Data>.*?Value="(.*?)" /></tt:Data>'
+    event_status_all = re.findall(pattern, xml_answer.decode('utf-8'))
 
-#	logging.debug(event_status_all)
-	for event_status in event_status_all:
-#		logging.debug(event_status)   
-		event=re.findall(r'tns1:.*/(.*)', event_status[0])
-		event="Ev" + event[0]
-#		logging.debug(event[0])
-		status=0
-		if(event_status[1]!="false"):
-			status=1
-		logging.debug(f"received event {event}  {status}")
-		eventFound=0
-		for eventName in eventTable:
-#			print(eventName)
-			if (eventName==event):
-				eventFound=1
-				if (eventTable[eventName]!=status):
-					logging.debug(f"{event} --> {status}")
-					eventTable[eventName]=status
-					send_frame = {
-						"message": event,
-						"ip": ip,
-						"motionstate": status
-					}
-        			# convert into JSON:
-					message = json.dumps(send_frame)
-					jeedom_cnx.send_change_immediate(json.loads(message))
-		if (eventFound==0):
-			eventTable[event] = status
-			logging.debug(f"{event} --> {status}")
-			send_frame = {
-				"message": event,
-				"ip": ip,
-				"motionstate": status
-			}
-			# convert into JSON:
-			message = json.dumps(send_frame)
-			jeedom_cnx.send_change_immediate(json.loads(message))
+    # logging.debug(event_status_all)
+    for event_status in event_status_all:
+        # logging.debug(event_status)
+        event = re.findall(r'tns1:.*/(.*)', event_status[0])
+        event = "Ev" + event[0]
+        # logging.debug(event[0])
+        status = 0
+        if event_status[1] != "false":
+            status = 1
+        logging.debug(f"received event {event}  {status}")
+        eventFound = 0
+        for eventName in eventTable:
+            # print(eventName)
+            if eventName == event:
+                eventFound = 1
+                if (eventTable[eventName] != status):
+                    logging.debug(f"{event} --> {status}")
+                    eventTable[eventName] = status
+                    send_frame = {
+                        "message": event,
+                        "ip": ip,
+                        "motionstate": status
+                    }
+                    # convert into JSON:
+                    message = json.dumps(send_frame)
+                    jeedom_cnx.send_change_immediate(json.loads(message))
+        if eventFound == 0:
+            eventTable[event] = status
+            logging.debug(f"{event} --> {status}")
+            send_frame = {
+                "message": event,
+                "ip": ip,
+                "motionstate": status
+            }
+            # convert into JSON:
+            message = json.dumps(send_frame)
+            jeedom_cnx.send_change_immediate(json.loads(message))
 # END  added by t0urista to handle ONVIF events
 
-	return
+    return
--- resources/demond/jeedom/jeedom.py
+++ resources/demond/jeedom/jeedom.py
@@ -14,69 +14,45 @@
 # along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
 #
 
-
+import time
 import logging
-import threading
+from threading import Thread
 import requests
-import datetime
-import collections
+from collections.abc import Mapping
 import os
 from queue import Queue
 import socketserver
 from socketserver import (TCPServer, StreamRequestHandler)
+import unicodedata
 
 
-# ------------------------------------------------------------------------------
-
-class jeedom_com:
+class jeedom_com():
     def __init__(self, apikey='', url='', cycle=0.5, retry=3):
-        self.apikey = apikey
-        self.url = url
-        self.cycle = cycle
-        self.retry = retry
-        self.changes = {}
-        if cycle > 0:
-            self.send_changes_async()
-        logging.debug('Init request module v%s' % (str(requests.__version__),))
-    
-    def send_changes_async(self):
-        try:
-            if len(self.changes) == 0:
-                resend_changes = threading.Timer(self.cycle, self.send_changes_async)
-                resend_changes.start()
-                return
-            start_time = datetime.datetime.now()
-            changes = self.changes
-            self.changes = {}
-            logging.debug('Send to jeedom : ' + str(changes))
-            i = 0
-            while i < self.retry:
-                try:
-                    r = requests.post(self.url + '?apikey=' + self.apikey, json=changes, timeout=(0.5, 120),
-                                      verify=False)
-                    if r.status_code == requests.codes.ok:
-                        break
-                except Exception as error:
-                    logging.error(
-                        'Error on send request to jeedom ' + str(error) + ' retry : ' + str(i) + '/' + str(self.retry))
-                i = i + 1
-            if r.status_code != requests.codes.ok:
-                logging.error('Error on send request to jeedom, return code %s' % (str(r.status_code),))
-            dt = datetime.datetime.now() - start_time
-            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
-            timer_duration = self.cycle - ms
-            if timer_duration < 0.1:
-                timer_duration = 0.1
-            if timer_duration > self.cycle:
-                timer_duration = self.cycle
-            resend_changes = threading.Timer(timer_duration, self.send_changes_async)
-            resend_changes.start()
-        except Exception as error:
-            logging.error('Critical error on  send_changes_async %s' % (str(error),))
-            resend_changes = threading.Timer(self.cycle, self.send_changes_async)
-            resend_changes.start()
-    
-    def add_changes(self, key, value):
+        self._apikey = apikey
+        self._url = url
+        self._cycle = cycle
+        self._retry = retry
+        self._changes = {}
+        if self._cycle > 0:
+            Thread(target=self.__thread_changes_async, daemon=True).start()
+        logging.info('Init request module v%s', requests.__version__)
+
+    def __thread_changes_async(self):
+        if self._cycle <= 0:
+            return
+        logging.info('Start changes async thread')
+        while True:
+            try:
+                time.sleep(self._cycle)
+                if len(self._changes) == 0:
+                    continue
+                changes = self._changes
+                self._changes = {}
+                self.__post_change(changes)
+            except Exception as error:
+                logging.error('Critical error on send_changes_async %s', error)
+
+    def add_changes(self, key: str, value):
         if key.find('::') != -1:
             tmp_changes = {}
             changes = value
@@ -86,103 +62,139 @@ class jeedom_com:
                 tmp_changes[k] = changes
                 changes = tmp_changes
                 tmp_changes = {}
-            if self.cycle <= 0:
+            if self._cycle <= 0:
                 self.send_change_immediate(changes)
             else:
-                self.merge_dict(self.changes, changes)
+                self.merge_dict(self._changes, changes)
         else:
-            if self.cycle <= 0:
+            if self._cycle <= 0:
                 self.send_change_immediate({key: value})
             else:
-                self.changes[key] = value
-    
+                self._changes[key] = value
+
     def send_change_immediate(self, change):
-        threading.Thread(target=self.thread_change, args=(change,)).start()
-    
-    def thread_change(self, change):
-        logging.debug('Send to jeedom :  %s' % (str(change),))
-        i = 0
-        while i < self.retry:
+        Thread(target=self.__post_change, args=(change,)).start()
+
+    def __post_change(self, change):
+        logging.debug('Send to jeedom: %s', change)
+        for i in range(self._retry):
             try:
-                r = requests.post(self.url + '?apikey=' + self.apikey, json=change, timeout=(0.5, 120), verify=False)
+                r = requests.post(self._url + '?apikey=' + self._apikey, json=change, timeout=(0.5, 120), verify=False)
                 if r.status_code == requests.codes.ok:
-                    break
+                    return True
+                else:
+                    logging.warning('Error on send request to jeedom, return code %s', r.status_code)
             except Exception as error:
-                logging.error(
-                    'Error on send request to jeedom ' + str(error) + ' retry : ' + str(i) + '/' + str(self.retry))
-            i = i + 1
-    
+                logging.error('Error on send request to jeedom "%s" retry: %i/%i', error, i, self._retry)
+            time.sleep(0.5)
+        return False
+
     def set_change(self, changes):
-        self.changes = changes
-    
+        self._changes = changes
+
     def get_change(self):
-        return self.changes
-    
+        return self._changes
+
     def merge_dict(self, d1, d2):
         for k, v2 in d2.items():
             v1 = d1.get(k)  # returns None if v1 has no value for this key
-            if (isinstance(v1, collections.Mapping) and
-                    isinstance(v2, collections.Mapping)):
+            if isinstance(v1, Mapping) and isinstance(v2, Mapping):
                 self.merge_dict(v1, v2)
             else:
                 d1[k] = v2
-    
+
     def test(self):
         try:
-            response = requests.get(self.url + '?apikey=' + self.apikey, verify=False)
+            response = requests.get(self._url + '?apikey=' + self._apikey, verify=False)
             if response.status_code != requests.codes.ok:
-                logging.error('Callback error: %s %s. Please check your network configuration page'
-                              % (response.status_code, response.reason,))
-                logging.error(response.text)
+                logging.error('Callback error: %s %s. Please check your network configuration page', response.status_code, response.reason)
                 return False
         except Exception as e:
-            logging.error(
-                'Callback result as a unknown error: %s. Please check your network configuration page' % (e.message,))
+            logging.error('Callback result as a unknown error: %s. Please check your network configuration page', e)
             return False
         return True
 
 
-# ------------------------------------------------------------------------------
+class jeedom_utils():
 
-class jeedom_utils:
-    
     @staticmethod
     def convert_log_level(level='error'):
-        log_lvl = {'debug': logging.DEBUG,
-                   'info': logging.INFO,
-                   'notice': logging.WARNING,
-                   'warning': logging.WARNING,
-                   'error': logging.ERROR,
-                   'critical': logging.CRITICAL,
-                   'none': logging.CRITICAL}
-        return log_lvl.get(level, logging.CRITICAL)
-    
+        LEVELS = {
+            'debug': logging.DEBUG,
+            'info': logging.INFO,
+            'notice': logging.WARNING,
+            'warning': logging.WARNING,
+            'error': logging.ERROR,
+            'critical': logging.CRITICAL,
+            'none': logging.CRITICAL
+            }
+        return LEVELS.get(level, logging.CRITICAL)
+
     @staticmethod
     def set_log_level(level='error'):
-        log_format = '[%(asctime)-15s][%(levelname)s] : %(message)s'
-        logging.basicConfig(level=jeedom_utils.convert_log_level(level), format=log_format, datefmt="%Y-%m-%d %H:%M:%S")
-    
+        FORMAT = '[%(asctime)-15s][%(levelname)s] : %(message)s'
+        logging.basicConfig(level=jeedom_utils.convert_log_level(level), format=FORMAT, datefmt="%Y-%m-%d %H:%M:%S")
+
+    @staticmethod
+    def stripped(str):
+        return "".join([i for i in str if i in range(32, 127)])
+
+    @staticmethod
+    def ByteToHex(byteStr: bytes):
+        return byteStr.hex()
+
+    @staticmethod
+    def dec2bin(x, width=8):
+        return ''.join(str((x >> i) & 1) for i in range(width-1, -1, -1))
+
+    @staticmethod
+    def dec2hex(dec):
+        if dec is None:
+            return '0x00'
+        return "0x{:02X}".format(dec)
+
+    @staticmethod
+    def testBit(int_type, offset):
+        mask = 1 << offset
+        return (int_type & mask)
+
+    @staticmethod
+    def clearBit(int_type, offset):
+        mask = ~(1 << offset)
+        return (int_type & mask)
+
+    @staticmethod
+    def split_len(seq, length):
+        return [seq[i:i+length] for i in range(0, len(seq), length)]
+
     @staticmethod
     def write_pid(path):
         pid = str(os.getpid())
-        logging.debug("Writing PID " + pid + " to " + str(path))
+        logging.info("Writing PID %s to %s", pid, path)
         open(path, 'w').write("%s\n" % pid)
 
+    @staticmethod
+    def remove_accents(input_str: str):
+        nkfd_form = unicodedata.normalize('NFKD', input_str)
+        return u"".join([c for c in nkfd_form if not unicodedata.combining(c)])
 
-# ------------------------------------------------------------------------------
+    @staticmethod
+    def printHex(hex):
+        return ' '.join([hex[i:i + 2] for i in range(0, len(hex), 2)])
+
 
 JEEDOM_SOCKET_MESSAGE = Queue()
 
 
-class JeedomSocketHandler(StreamRequestHandler):
+class jeedom_socket_handler(StreamRequestHandler):
     def handle(self):
         global JEEDOM_SOCKET_MESSAGE
-        logging.debug("Client connected to [%s:%d]" % self.client_address)
+        logging.info("Client connected to [%s:%d]", self.client_address[0], self.client_address[1])
         lg = self.rfile.readline()
         JEEDOM_SOCKET_MESSAGE.put(lg)
-        # logging.debug("Message read from socket: " + str(lg.strip()))
+        logging.info("Message read from socket: %s", str(lg.strip()))
         self.netAdapterClientConnected = False
-        logging.debug("Client disconnected from [%s:%d]" % self.client_address)
+        logging.info("Client disconnected from [%s:%d]", self.client_address[0], self.client_address[1])
 
 
 class jeedom_socket():
@@ -193,25 +205,21 @@ class jeedom_socket():
         socketserver.TCPServer.allow_reuse_address = True
 
     def open(self):
-        self.netAdapter = TCPServer((self.address, self.port), JeedomSocketHandler)
+        self.netAdapter = TCPServer((self.address, self.port), jeedom_socket_handler)
         if self.netAdapter:
-            logging.debug("Socket interface started")
-            threading.Thread(target=self.loopNetServer, args=()).start()
+            logging.info("Socket interface started")
+            Thread(target=self.loopNetServer).start()
         else:
-            logging.debug("Cannot start socket interface")
+            logging.info("Cannot start socket interface")
 
     def loopNetServer(self):
-        logging.debug("LoopNetServer Thread started")
-        logging.debug("Listening on: [%s:%d]" % (self.address, self.port))
+        logging.info("LoopNetServer Thread started")
+        logging.info("Listening on: [%s:%d]", self.address, self.port)
         self.netAdapter.serve_forever()
-        logging.debug("LoopNetServer Thread stopped")
+        logging.info("LoopNetServer Thread stopped")
 
     def close(self):
         self.netAdapter.shutdown()
 
     def getMessage(self):
         return self.message
-
-# ------------------------------------------------------------------------------
-# END
-# ------------------------------------------------------------------------------
